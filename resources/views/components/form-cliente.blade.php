2d2ecd110158f428b340a28f6d3ef0d00e0018b1
vitao <gvictor567@gmail.com>
2024-01-10 19:24:12 -0300

    feat: tela de clientes iniciada

---
+++ resources/views/components/form-cliente.blade.php
@@ -0,0 +1,163 @@
+<form class="create-cliente-component" id="cadastroForm" method="POST" action="{{ route('cliente.post') }}">
+    @csrf
+    <div class="form-inputs-container">
+        <label>Nome:<span style="color: red"> *</span></label>
+        <input placeholder="Nome" name="nome" required type="text">
+        <label>Situação:<span style="color: red"> *</span></label>
+        <input placeholder="Situação" name="situacao" required type="text">
+        <label>Naturalidade:<span style="color: red"> *</span></label>
+        <select id="naturalidade" name="naturalidade" onchange="atualizarEstadoCivil(); ocultarCampoOutro()">
+            <option id="brasileiro" value="brasileiro">brasileiro</option>
+            <option id="brasileira" value="brasileira">brasileira</option>
+            <option value="outro">Outro</option>
+        </select>
+        <div id="campoOutro" style="display: none;">
+            <label for="outro">Digite a opção:</label>
+            <input type="text" id="outroCampo" name="outroCampo">
+        </div>
+        <label>Estado Civil:<span style="color: red"> *</span></label>
+        <select id="estado_civil" name="estado_civil">
+        </select>
+        <label>Profissão:<span style="color: red"> *</span></label>
+        <input placeholder="Profissão" name="profissao" required type="text">
+        <label>Nome da Mãe:<span style="color: red"> *</span></label>
+        <input placeholder="Mãe" name="nome_mae" required type="text">
+        <label>Nome do Pai: </label>
+        <input placeholder="Pai" name="nome_pai" required type="text">
+        <label>RG:<span style="color: red"> *</span></label>
+        <input placeholder="99.999.999-99" id="rg" name="rg" required type="text">
+        <label>CPF:<span style="color: red"> *</span></label>
+        <input placeholder="999.999.999-99" id="cpf" name="cpf" required type="text">
+        <label>Celular:</label>
+        <input type="text" id="celular" name="celular" placeholder="(99) 99999-9999">
+        <label>Cidade de Nascimento:<span style="color: red"> *</span></label>
+        <input placeholder="Cidade" name="cidade_nascimento" required type="text">
+        <label>Estado de Nascimento:<span style="color: red"> *</span></label>
+        <input placeholder="Estado" name="estado_nascimento" required type="text">
+        <label>Data de Nascimento:<span style="color: red"> *</span></label>
+        <input placeholder="99/99/9999" id="nascimento" name="nascimento" required type="text">
+        <label class="label-endereco">Endereço:<span style="color: red"> *</span></label>
+        <div class="cliente-endereco-container">
+            <div class="endereco-section primeiro">
+                <h6>CEP:<span style="color: red"> *</span></h6>
+                <input id="cep" placeholder="CEP" name="cep" required type="text">
+            </div>
+            <div class="endereco-section segundo">
+                <h6>Rua:<span style="color: red"> *</span></h6>
+                <input id="logradouro" placeholder="Rua" name="rua" required type="text">
+            </div>
+            <div class="endereco-section terceiro">
+                <h6>Bairro:<span style="color: red"> *</span></h6>
+                <input id="bairro" name="bairro" required type="text">
+            </div>
+            <div class="endereco-section quarto">
+                <h6>Número:<span style="color: red"> *</span></h6>
+                <input id="numero" name="numero" required type="text">
+            </div>
+            <div class="endereco-section quinto">
+                <h6>Estado:<span style="color: red"> *</span></h6>
+                <input id="estado" name="estado" required type="text">
+            </div>
+            <div class="endereco-section sexto">
+                <h6>Cidade:<span style="color: red"> *</span></h6>
+                <input id="cidade" name="cidade" required type="text">
+            </div>
+        </div>
+    </div>
+
+
+    <button type="submit" id="btnCadastrarCliente" onclick="exibirDados()">
+        <p>CADASTRAR</p>
+    </button>
+
+</form>
+
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.inputmask/5.0.6/jquery.inputmask.min.js"></script>
+<script>
+    $(document).ready(function () {
+        $('#celular').inputmask('(99) 99999-9999', { placeholder: ' ' });
+        $('#rg').inputmask('99.999.999-99', { placeholder: ' ' });
+        $('#cpf').inputmask('999.999.999-99', { placeholder: ' ' });
+        $('#nascimento').inputmask('99/99/9999', { placeholder: ' ' });
+
+    });
+
+    function atualizarEstadoCivil() {
+
+        let estadoCivilSelect = document.getElementById('estado_civil');
+        estadoCivilSelect.innerHTML = '';
+
+        let naturalidadeSelect = document.getElementById('naturalidade');
+        let naturalidadeValue = naturalidadeSelect.value;
+
+        if (naturalidadeValue === 'brasileiro') {
+            adicionarOpcao('solteiro', 'solteiro');
+            adicionarOpcao('casado', 'casado');
+            adicionarOpcao('divorciado', 'divorciado');
+            adicionarOpcao('viuvo', 'viúvo');
+        } else if (naturalidadeValue === 'brasileira') {
+            adicionarOpcao('solteira', 'solteira');
+            adicionarOpcao('casada', 'casada');
+            adicionarOpcao('divorciada', 'divorciada');
+            adicionarOpcao('viuva', 'viúva');
+        } else if (naturalidadeValue === 'outro') {
+            mostrarCampoOutro();
+            adicionarOpcao('solteiro(a)', 'solteiro(a)');
+            adicionarOpcao('casado(a)', 'casado(a)');
+            adicionarOpcao('divorciado(a)', 'divorciado(a)');
+            adicionarOpcao('viuvo(a)', 'viúvo(a)');
+        }
+
+    }
+
+    function adicionarOpcao(value, text) {
+        let option = document.createElement('option');
+        option.value = value;
+        option.text = text;
+        document.getElementById('estado_civil').appendChild(option);
+    }
+
+    function mostrarCampoOutro() {
+        let select = document.getElementById('naturalidade');
+        let campoOutro = document.getElementById('campoOutro');
+
+        campoOutro.style.display = select.value === 'outro' ? 'block' : 'none';
+
+        if (select.value !== 'outro') {
+            document.getElementById('outroCampo').value = '';
+        }
+    }
+
+    function ocultarCampoOutro() {
+        var select = document.getElementById('naturalidade');
+        var campoOutro = document.getElementById('campoOutro');
+
+        if (select.value === 'brasileiro' || select.value === 'brasileira') {
+            campoOutro.style.display = 'none';
+            document.getElementById('outroCampo').value = '';
+        } else {
+            campoOutro.style.display = 'block';
+        }
+    } atualizarEstadoCivil();
+
+    $("#cep").focusout(function () {
+        $.ajax({
+
+            url: 'https://viacep.com.br/ws/' + $(this).val() + '/json/',
+            dataType: 'json',
+            success: function (resposta) {
+
+                $("#logradouro").val(resposta.logradouro);
+                $("#numero").val(resposta.complemento);
+                $("#bairro").val(resposta.bairro);
+                $("#cidade").val(resposta.localidade);
+                $("#estado").val(resposta.uf);
+                $("#cep").val(resposta.cep);
+                $("#numero").focus();
+            }
+        });
+
+    });
+
+</script>
--- resources/views/header/clientes.blade.php
+++ resources/views/header/clientes.blade.php
@@ -1,3 +1,136 @@
-<div>
-    <!-- He who is contented is rich. - Laozi -->
-</div>
+<x-app-layout>
+    <div class="clients-content">
+        <header class="clients-header">
+            <p>Clientes</p>
+            <input type="text" id="searchInput" placeholder="Pesquisar por nome...">
+            <div class="add-client-icon">+</div>
+        </header>
+        <div class="no-active-x clients-right">
+            <div class="close-client-icon">➠</div>
+            <p class='form-title'>Registrar Cliente</p>
+            <x-form-cliente />
+        </div>
+        <div class="no-active-opacity client-opacity-bg"></div>
+        <div class="all-clients" id="searchClients">
+            @foreach($clientes as $cliente)
+            <div class="client-container searchable-div">
+                <p class="searchable-name">{{$cliente->nome}} <span>({{$cliente->situacao}})</span></p>
+                <div class="icon-container">
+                    <div class="arrow-icon">➠</div>
+                </div>
+            </div>
+            <div class="client-info">
+                <div class="client-data">
+                    <div class="client-data-left">
+                        <h6>Nome da Mãe: <span>{{$cliente->nome_mae}}</span></h6>
+                        <h6>Nome do Pai: <span>{{$cliente->nome_pai}}</span></h6>
+                        <h6>Naturalidade: <span>{{$cliente->naturalidade}}</span></h6>
+                        <h6>Estado Civil: <span>{{$cliente->estado_civil}}</span></h6>
+                        <h6>Profissão: <span>{{$cliente->profissao}}</span></h6>
+                        <h6>RG: <span>{{$cliente->rg}}</span></h6>
+                        <h6>CPF: <span>{{$cliente->cpf}}</span></h6>
+                        <h6>Data de Nascimento: <span>{{$cliente->nascimento}}</span></h6>
+                        <h6>Local de Nascimento: <span>{{$cliente->cidade_nascimento}} /
+                                {{$cliente->estado_nascimento}}</span></h6>
+                        <h6>Celular: <span>{{$cliente->celular}}</span></h6>
+                    </div>
+                    <div class="client-data-right">
+                        <h5>Endereço:</h5>
+                        <div class="adress-content">
+                            <h6>CEP: <span>{{$cliente->endereco['cep']}}</span></h6>
+                            <h6>Rua: <span>{{$cliente->endereco['rua']}}</span></h6>
+                            <h6>Bairro: <span>{{$cliente->endereco['bairro']}}</span></h6>
+                            <h6>Número: <span>{{$cliente->endereco['numero']}}</span></h6>
+                            <h6>Cidade: <span>{{$cliente->endereco['cidade']}}</span></h6>
+                            <h6>Estado: <span>{{$cliente->endereco['estado']}}</span></h6>
+                        </div>
+                    </div>
+                </div>
+                <div class="client-docs">
+                    <h4>Documentos :</h4>
+                    <fonm class="add-document" method="POST" action="{{ route('cliente.post') }}">
+                        @csrf
+                        <label>Nome Do Arquivo:<span style="color: red"> *</span></label>
+                        <input placeholder="nome" name="nome" required type="text">
+                        <label>Descrição:<span style="color: red"> *</span></label>
+                        <input placeholder="Descrição" name="descricao" required type="text" class="doc-text">
+                        <div class="upload-doc">JORGE</div>
+                        <button type="submit">
+                        </button>
+                    </fonm>
+                    <div class="doc-icon"></div>
+                </div>
+            </div>
+            @endforeach
+        </div>
+    </div>
+</x-app-layout>
+
+<script>
+
+    document.addEventListener('DOMContentLoaded', function () {
+        document.querySelectorAll('.add-client-icon').forEach(function (icon) {
+            icon.addEventListener('click', function () {
+                let appointmentsRight = document.querySelector('.clients-right');
+                let opacityBg = document.querySelector('.client-opacity-bg');
+
+                appointmentsRight.classList.toggle('active-x');
+                appointmentsRight.classList.toggle('no-active-x');
+                opacityBg.classList.toggle('active-opacity');
+                opacityBg.classList.toggle('no-active-opacity');
+
+            });
+        });
+
+        document.querySelectorAll('.close-client-icon').forEach(function (icon) {
+            icon.addEventListener('click', function () {
+                let appointmentsRight = document.querySelector('.clients-right');
+                let opacityBg = document.querySelector('.client-opacity-bg');
+
+                appointmentsRight.classList.toggle('active-x');
+                appointmentsRight.classList.toggle('no-active-x');
+                opacityBg.classList.toggle('active-opacity');
+                opacityBg.classList.toggle('no-active-opacity');
+
+            });
+        });
+
+        document.getElementById('searchInput').addEventListener('input', function () {
+            let searchTerm = this.value.toLowerCase();
+            let searchResults = document.getElementById('searchClients');
+            let items = searchResults.getElementsByClassName('searchable-div');
+            let names = searchResults.getElementsByClassName('searchable-name');
+
+            for (let i = 0; i < items.length; i++) {
+                let name = names[i];
+                let item = items[i];
+
+                let textContent = name.textContent || name.innerText;
+
+                if (textContent.toLowerCase().includes(searchTerm)) {
+                    item.style.display = 'block';
+                } else {
+                    item.style.display = 'none';
+                }
+            }
+        });
+
+    });
+
+    document.addEventListener('DOMContentLoaded', function () {
+        var arrowIcons = document.querySelectorAll('.arrow-icon');
+
+        arrowIcons.forEach(function (arrowIcon) {
+            arrowIcon.addEventListener('click', function () {
+                var clientInfo = this.closest('.client-container').nextElementSibling;
+
+                if (clientInfo.style.height === '0px' || clientInfo.style.height === '') {
+                    clientInfo.style.height = clientInfo.scrollHeight + 'px';
+                } else {
+                    clientInfo.style.height = '0';
+                }
+            });
+        });
+    });
+
+</script>
