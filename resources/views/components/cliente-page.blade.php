4a8ce5feeff8b41e53b43968b4eb2d20911ac243
vitao <gvictor567@gmail.com>
2024-02-01 14:10:30 -0300

    fix: anexos de documentos dos clientes

--- resources/views/components/cliente-page.blade.php
+++ resources/views/components/cliente-page.blade.php
@@ -20,11 +20,91 @@
             <h5><strong>Local de Nascimento: </strong>{{$cliente->cidade_nascimento}} / {{$cliente->estado_nascimento}}
             </h5>
         </div>
-        <div class="cliente-content-right">
+        <ul class="cliente-content-right">
             <h6>Documentos: </h6>
-            <span class="add-document">
+            @foreach($cliente->documentos as $docs)
+            <li>
+                <div>
+                    <a href="{{$docs['url']}}" target="_blank">
+                        @if($docs['type'] === 'pdf')
+                        <x-pdf-gray />
+                        @else
+                        <x-image-icon />
+                        @endif
+                    </a>
+                    <p>{{ $docs['nome'] }}</p>
+                </div>
+                <h6>({{$docs['descricao']}})</h6>
+            </li>
+            @endforeach
+            <span class="add-pdf-icon">
                 <x-add-pdf />
             </span>
-        </div>
+        </ul>
     </div>
 </div>
+
+<div class="add-document-container close-add-document">
+    <form method="POST" action="{{ route('cliente.doc') }}" enctype="multipart/form-data">
+        @csrf
+        <label for="arquivos">Selecione os documentos:</label>
+        <input type="hidden" name="cliente_id" value="{{ $cliente->id }}" />
+        <div class="input-files-container">
+            <input class="arquivos input-files" type="file" name="arquivos[]" accept=".pdf, .png, .jpg, .jpeg" required
+                multiple>
+            <p>Clique aqui para adicionar os arquivos</p>
+        </div>
+        <div class="camposExtras"></div>
+        <input class="input-button" type="submit" value="Enviar">
+        <div class="close-form-document">X</div>
+    </form>
+</div>
+
+<script>
+    document.addEventListener('DOMContentLoaded', function () {
+        document.querySelectorAll('.add-pdf-icon').forEach(function (icon) {
+            icon.addEventListener('click', function () {
+                let addDocument = document.querySelector('.add-document-container');
+                addDocument.classList.toggle('open-add-document');
+                addDocument.classList.toggle('close-add-document');
+            });
+        });
+
+        document.querySelectorAll('.close-form-document').forEach(function (icon) {
+            icon.addEventListener('click', function () {
+                let addDocument = document.querySelector('.add-document-container');
+                addDocument.classList.toggle('open-add-document');
+                addDocument.classList.toggle('close-add-document');
+            });
+        });
+
+        // Adiciona um evento de mudança ao campo de entrada de arquivo
+        document.querySelector('.input-files').addEventListener('change', function (event) {
+            // Obtém os arquivos selecionados
+            const files = event.target.files;
+
+            // Cria campos de entrada adicionais para nome e descrição para cada arquivo
+            const camposExtras = document.querySelector('.camposExtras');
+            camposExtras.innerHTML = ''; // Limpa os campos extras antes de adicionar novos
+
+            for (let i = 0; i < files.length; i++) {
+                const file = files[i];
+
+                // Cria campos de entrada para nome e descrição
+                const nomeInput = document.createElement('input');
+                nomeInput.type = 'text';
+                nomeInput.name = `nomes[${i}]`; // Use um array para enviar vários nomes ao backend
+                nomeInput.placeholder = 'Nome do Documento';
+
+                const descricaoInput = document.createElement('input');
+                descricaoInput.type = 'text';
+                descricaoInput.name = `descricoes[${i}]`; // Use um array para enviar várias descrições ao backend
+                descricaoInput.placeholder = 'Descrição do Documento';
+
+                // Adiciona os campos ao formulário
+                camposExtras.appendChild(nomeInput);
+                camposExtras.appendChild(descricaoInput);
+            }
+        });
+    });
+</script>
