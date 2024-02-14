0eaf662a7854cd4a1be66d2e5a03f77a160f0634
vitao <gvictor567@gmail.com>
2024-02-14 12:40:13 -0300

    feat: footer

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,4 +1,5 @@
 <x-app-layout>
+    <div class="dashboard-main-container">
     <div class="dashboard-container">
         <h1>Agendamentos</h1>
         <div class="dashboard-updates">
@@ -21,5 +22,15 @@
             <div class="dashboard-notifications">
             </div>
         </div>
+        <h1>Alguma Coisa</h1>
+        <div class="alguma-coisa"></div>
     </div>
+    <footer class="dashboard-footer">
+        <div class="dashboard-footer-logo">
+            <x-vg-nome/>
+            <p>Advogados Associados</p>
+        </div>
+    </footer>
+    </div>
+    
 </x-app-layout>
